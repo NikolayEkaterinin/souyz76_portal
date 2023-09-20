3f7222ed55776f953b4799538591fdb248b46d69
Николай Екатеринин <eh37@ya.ru>
2023-09-20 15:59:13 +0300

    Добавлен .gitignore для игнорирования secret_key.py

--- soyuz76_portal/soyuz76_portal/settings.py
+++ soyuz76_portal/soyuz76_portal/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
+from secret_key import SECRET_KEY
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+g@&=f_2)$ny_sm!+yav54risfyw&-6t6ffshmhl)585ad_q6n'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
